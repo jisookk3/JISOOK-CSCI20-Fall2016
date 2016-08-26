368a683cc84f73db10ac86c82c41c743c0ce3b13
Jisook Kim <jkim014@student.butte.edu>
2016-08-26 19:20:08 +0000

    ASCII art

---
+++ lab1/lab1.cpp
@@ -0,0 +1,21 @@
+
+/* 1.1 Lab - ASCII Art
+ * Date: Aug 25 2016
+ * Creator: Jisook Kim
+*/ 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "     ^------------^ " << endl;
+    cout << "    *              *" << endl;
+    cout << " ~~*~   (@)  (@)   ~*~~" << endl;
+    cout << " ~~*~       &      ~*~~" << endl;
+    cout << "    *     -----    *" << endl;
+    cout << "     * ===========*" << endl;
+    cout << "          &&&&" << endl;
+    
+    return 0;
+}
