ee2770712a05e7127a4cb2d0275a92c216a4195a
Jisook Kim <jkim014@student.butte.edu>
2016-08-29 04:01:51 +0000

    lab2 submission

---
+++ lab2/lab2.cpp
@@ -0,0 +1,28 @@
+
+/* 1.2 Lab - Algorithm - A simple number guessing game
+* Date: Aug 28 2016
+ 
+* Creator: Jisook Kim 
+*/
+
+/*
+1. Set the variable 'Com_num'
+2. Set the variable 'User_num'
+3. Set the variable 'Score' and save 10 points to 'Score'.
+
+4. Give the computer a number range.  ex) 1~20
+5. The computer selects a number randomly between the range.
+6. Save the number computer selected to 'Com_num'.
+7. Ask a user and get the number a user inputs(guesses) and save it to 'User_num'.
+
+8. Comparison loop: Compare 'Com_num' with 'User_num'.
+
+If User_num is higher than Com_num, tell the user "Higher" and lower 1 point from 'Score'.
+If User_num is lower than Com_num, tell the user "Lower" and lower 1 point from 'Score'.
+If two numbers are identical, get out of the comparison loop and print out "You win!" and the score (the value of 'Score').
+
+Repeat the comparison loop until the value of 'Score' becomes zero. (Thus, it can be repeated 10 times at most.)
+
+9. If the Score is zero, print out "You lose!"
+
+*/
